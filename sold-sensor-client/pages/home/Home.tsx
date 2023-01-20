ce00e7496ecd9a569285ce03e4b010d5b3a35c00
steven arellano <stevenarellano@stevens-MacBook-Pro.local>
2023-01-20 05:11:26 -0600

    client-reset

---
+++ sold-sensor-client/pages/home/Home.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react';
+import { useHome } from '../../api';
+import styles from '/styles/modules/Home.module.scss';
+
+
+function Home() {
+
+    return (
+        <div className="pageContainer" style={{ justifyContent: "center" }}>
+            welcome.
+        </div>
+    );
+}
+
+export default Home;
